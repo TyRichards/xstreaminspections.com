f1e5c0773f833a87df19fb74d42581e2b5b1e32d
Ty Richards <me@tyrichards.com>
2014-03-12 13:37:53 -0500

    replace wp-core with Paradoxcreative wp core

---
+++ public_html/README.md
@@ -0,0 +1,6 @@
+Paradox
+==========
+A Mobile & Web Firm founded by Ty Richards
+info@paradoxsites.com
+
+This is a private repo for our live marketing web site
--- public_html/wp-includes/version.php
+++ public_html/wp-includes/version.php
@@ -4,14 +4,14 @@
  *
  * @global string $wp_version
  */
-$wp_version = '3.8.1';
+$wp_version = '3.7.1';
 
 /**
  * Holds the WordPress DB revision, increments when changes are made to the WordPress DB schema.
  *
  * @global int $wp_db_version
  */
-$wp_db_version = 26691;
+$wp_db_version = 25824;
 
 /**
  * Holds the TinyMCE version
