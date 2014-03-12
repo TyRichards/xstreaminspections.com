6a62ad4a977e7a08e1b9c7ab75928f2a3e68dc0b
Ty Richards <me@tyrichards.com>
2014-03-12 11:17:26 -0500

    updated wp-config for DO Database

--- public_html/wp-config.php
+++ public_html/wp-config.php
@@ -19,10 +19,10 @@
 define('DB_NAME', 'wp_xstream');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'wp_xstream');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'D9KZdM8ejBRx');
+define('DB_PASSWORD', 'pdsiZE2ugU9m');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
