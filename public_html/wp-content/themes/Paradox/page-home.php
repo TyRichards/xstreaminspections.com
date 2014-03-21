c8a7d83b668a685728dda3a849177ede0865dcbb
Ty Richards <me@tyrichards.com>
2014-03-21 09:07:56 -0500

    Add home content back

--- public_html/wp-content/themes/Paradox/page-home.php
+++ public_html/wp-content/themes/Paradox/page-home.php
@@ -42,6 +42,52 @@ Template Name: Home
     </div>
 </section>
 
+<section class="primary-content">
+    <div class="container">
+        <section class="main-col page-content col-sm-7 container">
+            <div id="main" class="site-main" role="main">
+                <?php
+                while (have_posts()) {
+                  the_post();
+
+                  get_template_part('content', 'page');
+
+                  echo "\n\n";
+               // If comments are open or we have at least one comment, load up the comment template
+                  if (comments_open() || '0' != get_comments_number()) {
+                    comments_template();
+                  }
+
+                  echo "\n\n";
+
+                } //endwhile;
+                ?>
+                <a class="btn btn-danger" href="#">
+                    <i class="fa fa-phone fa-1x">&nbsp;&nbsp;&nbsp;</i>
+                    Call (972) 492-7920
+                </a>
+                    &nbsp;&nbsp;
+                <a class="btn btn-danger" href="/contact-us">
+                    <i class="fa fa-envelope fa-1x">&nbsp;&nbsp;&nbsp;</i>
+                    Email Us
+                </a>                  
+           </div>
+        </section>
+        <section class="sidebar-col sidebar-right sidebar col-sm-5">
+            <div class="col-sm-10 col-sm-offset-2">
+                <?php 
+                    if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+                    the_post_thumbnail('sidebar-thumb', array('class' => 'feature-image attachment-sidebar-thumb img-rounded' ));
+                    } 
+                    else {
+                        ?><img src="<?php echo get_template_directory_uri(); ?>/assets/images/sidebar-default.jpg" class="feature-image attachment-sidebar-thumb wp-post-image img-rounded" alt="<?php wp_title('|', true, 'right'); ?>"><?php
+                    }
+                ?>         
+                <?php get_sidebar('default'); ?>
+            </div>
+        </section>
+    </div>
+</section>
 
 
 <?php get_footer(); ?> 
